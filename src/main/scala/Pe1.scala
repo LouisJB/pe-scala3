e98ef26b71c60db0a1c0c334b4c7daac1c2eead5
Louis J B <chillipower.uk@gmail.com>
2024-08-16 21:46:30 -0400

    #3 - improve big O efficiency for problem 1

--- src/main/scala/Pe1.scala
+++ src/main/scala/Pe1.scala
@@ -4,6 +4,12 @@
 }
 
 object Pe1 {
-  def solve =
-    (3 until 1000).filter(x => x % 3 == 0 || x % 5 == 0).sum
+  import MathUtils.ArithmeticSeries._
+  val n = 999
+  def solve = {
+    val sumN = arithmeticSeqSum(n)
+    sumN(3) + sumN(5) - sumN(15)
+  }
+  def solveBf =
+    (3 to n).filter(x => x % 3 == 0 || x % 5 == 0).sum
 }
