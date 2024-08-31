74f477ba57adaed1aa644dadc5ff640efc8ac8f7
Louis J B <chillipower.uk@gmail.com>
2024-08-30 21:34:13 -0400

    Add alternative primes lazylist to compare performance

--- src/main/scala/MathUtils.scala
+++ src/main/scala/MathUtils.scala
@@ -1,9 +1,16 @@
+import scala.annotation.tailrec
 object MathUtils {
   import Math._
   object Primes {
     lazy val primes: LazyList[Int] = 2 #:: LazyList.from(3).filter { p =>
       primes.takeWhile(x => x * x <= p)
              .forall(p % _ > 0)
+    }
+
+    lazy val primes2 = {
+      def sieve(s: LazyList[Int]): LazyList[Int] = // not tailrec warning
+        s.head #:: sieve(s.tail filter (_ % s.head != 0))
+      sieve(LazyList.from(2))
     }
 
     def primeFactors(x: Int): List[Int] = {
@@ -28,4 +35,16 @@ object MathUtils {
       d * arithmeticSum(floorDiv(n, d))
   }
   def sqr(x : Int) = x * x
+}
+
+object PrmesApp {
+  import MathUtils.Primes._
+  import TimeUtils._
+  def main(args: Array[String]) = {
+    val n = 1000
+    val ps1 = timeMs(primes.take(n).toList.drop(n-10))
+    println(ps1)
+    val ps2 = timeMs(primes2.take(n).toList.drop(n-10))
+    println(ps2)
+  }
 }
